eb390a86b9ab39f2db49614316382baef37c8228
ThallysonDinizdebrito <thallysonfdinizdebrito@gmail.com>
2025-08-26 23:05:04 -0300

    correção do outputs

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -17,17 +17,20 @@ output "nome_resource_group" {
 
 # Output do nome da Storage Account
 output "nome_storage_account" {
-  value = azurerm_storage_account.sa.name
+  description = "Nome da Storage Account criada"
+  value       = azurerm_storage_account.conta_armazenamento.name
 }
 
 # Output do nome do container raw
 output "nome_container_raw" {
-  value = azurerm_storage_container.raw.name
+  description = "Nome do container RAW criado"
+  value       = azurerm_storage_container.container_raw.name
 }
 
 # Output do ID do Databricks Workspace
 output "id_workspace_databricks" {
-  value = azurerm_databricks_workspace.workspace.id
+  description = "ID do Databricks Workspace"
+  value       = azurerm_databricks_workspace.workspace.id
 }
 
 # Output da URL do Databricks Workspace
