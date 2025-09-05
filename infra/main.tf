62ce0cf95d6cd2732fcfbadf4bc325f4a5ba4610
ThallysonDinizdebrito <thallysonfdinizdebrito@gmail.com>
2025-09-05 12:58:56 -0300

    criação azure function 09

--- infra/main.tf
+++ infra/main.tf
@@ -3,17 +3,13 @@
 # Objetivo: Provisionar recursos Azure + Databricks + Azure Function
 ###############################
 
-# ===========================
 # Resource Group
-# ===========================
 resource "azurerm_resource_group" "grupo_principal" {
   name     = var.nome_do_grupo_de_recursos
   location = var.localizacao
 }
 
-# ===========================
 # Storage Account
-# ===========================
 resource "azurerm_storage_account" "conta_armazenamento" {
   name                     = var.nome_da_conta_de_armazenamento
   resource_group_name      = azurerm_resource_group.grupo_principal.name
@@ -22,18 +18,14 @@ resource "azurerm_storage_account" "conta_armazenamento" {
   account_replication_type = "LRS"
 }
 
-# ===========================
 # Container 'raw'
-# ===========================
 resource "azurerm_storage_container" "container_raw" {
   name                  = var.nome_do_container_raw
   storage_account_name  = azurerm_storage_account.conta_armazenamento.name
   container_access_type = "private"
 }
 
-# ===========================
 # Workspace Databricks
-# ===========================
 resource "azurerm_databricks_workspace" "workspace" {
   name                = "${var.nome_do_grupo_de_recursos}-databricks"
   resource_group_name = azurerm_resource_group.grupo_principal.name
@@ -41,21 +33,16 @@ resource "azurerm_databricks_workspace" "workspace" {
   sku                 = "premium"
 }
 
-# ===========================
-# Azure Function App Plan
-# ===========================
+# Azure Function App Plan (Linux)
 resource "azurerm_service_plan" "function_plan" {
   name                = "${var.nome_do_grupo_de_recursos}-func-plan"
   location            = azurerm_resource_group.grupo_principal.location
   resource_group_name = azurerm_resource_group.grupo_principal.name
   os_type             = "Linux"
-
-  sku_name = "Y1"  # Dynamic Consumption Plan
+  sku_name            = "Y1"  # Dynamic Consumption Plan
 }
 
-# ===========================
 # Azure Linux Function App
-# ===========================
 resource "azurerm_linux_function_app" "function_app" {
   name                       = "${var.nome_do_grupo_de_recursos}-func"
   location                   = azurerm_resource_group.grupo_principal.location
@@ -63,7 +50,6 @@ resource "azurerm_linux_function_app" "function_app" {
   service_plan_id            = azurerm_service_plan.function_plan.id
   storage_account_name       = azurerm_storage_account.conta_armazenamento.name
   storage_account_access_key = azurerm_storage_account.conta_armazenamento.primary_access_key
-  version                    = "~4"
 
   site_config {
     linux_fx_version = "Python|3.13"
