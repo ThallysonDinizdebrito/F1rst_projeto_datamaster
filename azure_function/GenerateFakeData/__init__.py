9e64319633995e5c390f71c3b660b5c01ea9aa49
ThallysonDinizdebrito <thallysonfdinizdebrito@gmail.com>
2025-09-05 23:27:10 -0300

    criação azure function 34

--- azure_function/GenerateFakeData/__init__.py
+++ azure_function/GenerateFakeData/__init__.py
@@ -1,40 +1,49 @@
+import random
+from faker import Faker
 import datetime
 import json
-import os
-import random
 import logging
-from faker import Faker
-from azure.storage.blob import BlobServiceClient
+import os
 import azure.functions as func
-
+from azure.identity import DefaultAzureCredential
+from azure.storage.blob import BlobServiceClient
 
 fake = Faker()
 
+# Timer trigger function
 def main(mytimer: func.TimerRequest) -> None:
-    utc_timestamp = datetime.datetime.utcnow().strftime("%Y%m%d_%H%M%S")
+    utc_timestamp = datetime.datetime.utcnow().replace(
+        tzinfo=datetime.timezone.utc).isoformat()
 
-    # Gera dados fake
-    data = {
+    logging.info('Python timer trigger function ran at %s', utc_timestamp)
+
+    try:
+        # Recupera o nome da conta de storage (definido no Terraform como APP SETTING)
+        account_name = os.environ["AZURE_STORAGE_ACCOUNT_NAME"]
+        container_name = "raw"
+
+        # Conecta no Storage via Managed Identity
+        account_url = f"https://{account_name}.blob.core.windows.net"
+        credential = DefaultAzureCredential()
+        blob_service_client = BlobServiceClient(account_url=account_url, credential=credential)
+
+        # Gera dados fake
+        data = {
         "id": random.randint(1, 10000),
         "name": fake.name(),
         "email": fake.email(),
         "created_at": utc_timestamp
-    }
+        }
+        
 
-    # Conexão com Blob Storage via Service Principal (usando env var)
-    connection_string = os.environ["AZURE_STORAGE_CONNECTION_STRING"]
-    blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+        blob_name = f"dados_{datetime.datetime.utcnow().strftime('%Y%m%d%H%M%S')}.json"
+        blob_data = json.dumps(data)
 
-    # Container "raw"
-    container_name = os.environ.get("RAW_CONTAINER_NAME", "raw")
-    blob_name = f"fake_data_{utc_timestamp}.json"
+        # Envia para o container raw
+        container_client = blob_service_client.get_container_client(container_name)
+        container_client.upload_blob(name=blob_name, data=blob_data, overwrite=True)
 
-    # Serializa JSON
-    file_content = json.dumps(data, indent=4, ensure_ascii=False)
+        logging.info(f"Arquivo {blob_name} enviado com sucesso para o container {container_name}!")
 
-    # Upload para o Blob
-    blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
-    blob_client.upload_blob(file_content, overwrite=True)
-
-    logging.info(f"Arquivo enviado para container '{container_name}' com nome '{blob_name}'")
-    logging.info(f"Conteúdo: {data}")
+    except Exception as e:
+        logging.error(f"Erro ao processar a função: {e}")
