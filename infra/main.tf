b59e2b8049209094c955fc86a9f805462641c6c1
ThallysonDinizdebrito <thallysonfdinizdebrito@gmail.com>
2025-09-08 13:27:26 -0300

    Criação e configuração das regras do eventgrid

--- infra/main.tf
+++ infra/main.tf
@@ -127,6 +127,27 @@ resource "azurerm_eventgrid_system_topic" "raw_topic" {
 
 
 # ===========================
+# Event Grid - Subscription para Function
+# ===========================
+resource "azurerm_eventgrid_event_subscription" "raw_to_function" {
+  name  = "${var.nome_do_grupo_de_recursos}-sub-func"  # Nome da subscription
+  scope = azurerm_eventgrid_system_topic.raw_topic.id  # Escopo: o tópico criado acima
+
+  included_event_types = ["Microsoft.Storage.BlobCreated"]  # Apenas eventos de "Blob criado"
+
+  # endpoint da Function
+  azure_function_endpoint {
+    function_id = "${azurerm_linux_function_app.function_validate.id}/functions/ValidateFakeData"
+  }
+
+  retry_policy {
+    max_delivery_attempts = 5   # Número máximo de tentativas caso falhe a entrega do evento
+    event_time_to_live    = 1440 # Tempo de vida do evento (em minutos, aqui 1 dia)
+  }
+}
+
+
+# ===========================
 # Linux Function App - Validação
 # ===========================
 resource "azurerm_linux_function_app" "function_validate" {
