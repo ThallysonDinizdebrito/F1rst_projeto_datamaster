080c2d64f6b53bc19ad6e7b91c00dbed41433527
ThallysonDinizdebrito <thallysonfdinizdebrito@gmail.com>
2025-09-09 17:06:07 -0300

    teste func event grid CloudEvents v1.0

--- azure_function/ValidacaoData/ValidateFakeData/__init__.py
+++ azure_function/ValidacaoData/ValidateFakeData/__init__.py
@@ -3,14 +3,18 @@ import azure.functions as func
 import json
 
 def main(event: func.EventGridEvent):
-    logging.info('EventGridEvent received')
+    logging.info('CloudEvent received')
 
-    # O evento tem dados do blob
-    data = event.get_json()
-    logging.info(f"Data: {json.dumps(data)}")
+    # O evento CloudEvent tem dados do blob em event.get_json()['data']
+    cloud_event = event.get_json()
+    logging.info(f"CloudEvent: {json.dumps(cloud_event)}")
 
+    data = cloud_event.get("data", {})
     blob_url = data.get("url")
     logging.info(f"Blob URL: {blob_url}")
 
-    # Aqui você pode baixar/processar o blob
-    # Ex: chamar sua função de validação passando a URL ou conteúdo
+    if blob_url:
+        # Aqui você pode chamar sua função de validação passando a URL ou conteúdo
+        logging.info(f"Pronto para processar o blob: {blob_url}")
+    else:
+        logging.warning("Nenhuma URL de blob encontrada no evento")
