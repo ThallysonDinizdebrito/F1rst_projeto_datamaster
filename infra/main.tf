1a2aa119077e3c81bbcd1d55db053992a8fd9517
ThallysonDinizdebrito <thallysonfdinizdebrito@gmail.com>
2025-09-10 13:25:14 -0300

    mais um teste

--- infra/main.tf
+++ infra/main.tf
@@ -201,8 +201,12 @@ resource "azurerm_eventgrid_event_subscription" "sub_func" {
   event_delivery_schema = "CloudEventSchemaV1_0"
 
   azure_function_endpoint {
-    function_id = "/subscriptions/97eb265c-59ce-4122-bbe4-98f0d58d9208/resourceGroups/rg-dev-projeto/providers/Microsoft.Web/sites/rg-dev-projeto-func-validate/functions/validate_fake_data"
+    function_id = "${azurerm_linux_function_app.function_validate.id}/functions/validate_fake_data"
   }
+
+  azure_function_endpoint {
+  function_id = "/subscriptions/97eb265c-59ce-4122-bbe4-98f0d58d9208/resourceGroups/rg-dev-projeto/providers/Microsoft.Web/sites/rg-dev-projeto-func-validate/functions/validate_fake_data"
+}
 
 storage_blob_dead_letter_destination {
   storage_account_id          = azurerm_storage_account.conta_armazenamento.id
