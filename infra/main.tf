d7c5497f8231eaa2977e494650ae48888c30d62c
ThallysonDinizdebrito <thallysonfdinizdebrito@gmail.com>
2025-09-10 13:28:55 -0300

    vamos ver

--- infra/main.tf
+++ infra/main.tf
@@ -200,8 +200,7 @@ resource "azurerm_eventgrid_event_subscription" "sub_func" {
   included_event_types  = ["Microsoft.Storage.BlobCreated"]
   event_delivery_schema = "CloudEventSchemaV1_0"
 
-
-  zure_function_endpoint {
+azure_function_endpoint {
   function_id = "/subscriptiaons/97eb265c-59ce-4122-bbe4-98f0d58d9208/resourceGroups/rg-dev-projeto/providers/Microsoft.Web/sites/rg-dev-projeto-func-validate/functions/validate_fake_data"
 }
 
