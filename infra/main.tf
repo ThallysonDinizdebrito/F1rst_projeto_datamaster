3b9b2f744f5d03435e617783bd1eb228e02c9882
ThallysonDinizdebrito <thallysonfdinizdebrito@gmail.com>
2025-09-10 23:36:48 -0300

    subscrition criação

--- infra/main.tf
+++ infra/main.tf
@@ -201,7 +201,7 @@ resource "azurerm_eventgrid_event_subscription" "sub_func" {
   event_delivery_schema = "CloudEventSchemaV1_0"
 
 azure_function_endpoint {
-  function_id = "/subscriptions/97eb265c-59ce-4122-bbe4-98f0d58d9208/resourceGroups/rg-dev-projeto/providers/Microsoft.Web/sites/rg-dev-projeto-func-validate/functions/functionvalidacao"
+  function_id = "/subscriptions/97eb265c-59ce-4122-bbe4-98f0d58d9208/resourceGroups/rg-dev-projeto/providers/Microsoft.Web/sites/rg-dev-projeto-func-init/functions/validate_fake_data"
 }
 
 
