abc84cf41f8076137fa403194c533e66432112c6
ThallysonDinizdebrito <thallysonfdinizdebrito@gmail.com>
2025-09-12 00:22:52 -0300

    deixa rodar

--- infra/main.tf
+++ infra/main.tf
@@ -116,35 +116,3 @@ resource "azurerm_linux_function_app" "function_validate" {
   }
 }
 
-# ========================================
-# EventGrid Topic existente (data block)
-# ========================================
-
-resource "azurerm_eventgrid_topic" "topic" {
-  name                = "rg-dev-projeto-topic"
-  resource_group_name = azurerm_resource_group.grupo_principal.name
-  location            = azurerm_resource_group.grupo_principal.location
-}
-
-resource "azurerm_eventgrid_event_subscription" "sub_func" {
-  name  = "testetetetets3"
-  scope = azurerm_eventgrid_topic.topic.id  # agora existe o recurso
-
-  event_delivery_schema = "CloudEventSchemaV1_0"
-
-  retry_policy {
-    event_time_to_live    = 1440
-    max_delivery_attempts = 30
-  }
-
-  azure_function_endpoint {
-    function_id = "${azurerm_linux_function_app.function_validate.id}/functions/validate_fake_data"
-  }
-
-  lifecycle {
-    ignore_changes = [
-      azure_function_endpoint,
-    ]
-  }
-}
-
