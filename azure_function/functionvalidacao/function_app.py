eaaf02a306b0f4ecad092d8a361e7ef79f4246af
ThallysonDinizdebrito <thallysonfdinizdebrito@gmail.com>
2025-09-14 15:11:11 -0300

    teste function

--- azure_function/functionvalidacao/function_app.py
+++ azure_function/functionvalidacao/function_app.py
@@ -1,27 +1,25 @@
 import logging
 import azure.functions as func
 import json
-import os
 from azure.storage.blob import BlobServiceClient
 from jsonschema import validate, ValidationError
+import os
 
-app = func.FunctionApp()
-
-# Configuração de storage
+# Configurações de container
 STORAGE_CONN_STR = os.getenv("AzureWebJobsStorage")
 RAW_CONTAINER = os.getenv("RAW_CONTAINER_NAME", "raw")
 VALIDADO_CONTAINER = "validado"
 REJEITADO_CONTAINER = "rejeitado"
 
-# Caminho ajustado para schema.json fora da pasta da Function
+# Carrega schema
 SCHEMA_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), "schema_json", "schema.json")
 with open(SCHEMA_PATH, "r") as f:
     SCHEMA = json.load(f)
 
-@app.event_grid_trigger(arg_name="azeventgrid")
-def validate_fake_data(azeventgrid: func.EventGridEvent):
+
+def main(azeventgrid: func.EventGridEvent):
     logging.info("Evento do Event Grid recebido.")
-    
+
     event_data = azeventgrid.get_json()
     blob_url = event_data.get("url")
     if not blob_url:
@@ -37,22 +35,11 @@ def validate_fake_data(azeventgrid: func.EventGridEvent):
         data = json.loads(blob_data)
     except Exception as e:
         logging.error(f"Erro ao ler JSON ou baixar blob: {e}")
-        mover_blob(blob_service, blob_name, blob_data if 'blob_data' in locals() else "", REJEITADO_CONTAINER)
         return
 
     try:
         validate(instance=data, schema=SCHEMA)
-        logging.info(f"JSON válido [OK SUCESS] - {blob_name} enviado para '{VALIDADO_CONTAINER}'")
-        mover_blob(blob_service, blob_name, blob_data, VALIDADO_CONTAINER)
+        logging.info(f"JSON válido [ OK] - {blob_name} enviado para '{VALIDADO_CONTAINER}'")
     except ValidationError as e:
-        logging.warning(f"JSON inválido [X error] - {blob_name} enviado para '{REJEITADO_CONTAINER}': {e.message}")
-        mover_blob(blob_service, blob_name, blob_data, REJEITADO_CONTAINER)
+        logging.warning(f"JSON inválido [X] - {blob_name}: {e.message}")
 
-
-def mover_blob(blob_service: BlobServiceClient, blob_name: str, content: str, destino: str):
-    try:
-        container_client = blob_service.get_container_client(destino)
-        container_client.upload_blob(name=blob_name, data=content, overwrite=True)
-        logging.info(f"Blob {blob_name} movido para {destino}")
-    except Exception as e:
-        logging.error(f"Erro ao mover blob {blob_name} para {destino}: {e}")
