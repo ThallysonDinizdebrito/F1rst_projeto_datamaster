53a9fb1656b6852a41a92b291824951ae8a26157
ThallysonDinizdebrito <thallysonfdinizdebrito@gmail.com>
2025-09-13 22:23:46 -0300

    teste function flow

--- infra/main.tf
+++ infra/main.tf
@@ -77,23 +77,30 @@ resource "azurerm_linux_function_app" "function_app" {
   }
 }
 
+
+
 # ===========================
-# Data Source da Function App existente
+# Function Apps (vazias) - func-init
 # ===========================
 data "azurerm_linux_function_app" "function_validate" {
-  name                = "${var.nome_do_grupo_de_recursos}-func-init"
-  resource_group_name = azurerm_resource_group.grupo_principal.name
-}
+  name                      = "${var.nome_do_grupo_de_recursos}-func-init"
+  location                  = azurerm_resource_group.grupo_principal.location
+  resource_group_name       = azurerm_resource_group.grupo_principal.name
+  service_plan_id           = azurerm_service_plan.function_plan.id
+  storage_account_name      = azurerm_storage_account.conta_armazenamento.name
+  storage_account_access_key= azurerm_storage_account.conta_armazenamento.primary_access_key
 
-# Exemplo de uso das infos
-output "function_validate_id" {
-  value = data.azurerm_linux_function_app.function_validate.id
-}
 
-output "function_validate_default_hostname" {
-  value = data.azurerm_linux_function_app.function_validate.default_hostname
+  app_settings = {
+    "RAW_CONTAINER_NAME"              = azurerm_storage_container.container_raw.name
+    "VALIDATED_CONTAINER_NAME"        = azurerm_storage_container.container_validado.name
+    "REJECTED_CONTAINER_NAME"         = azurerm_storage_container.container_rejeitados.name
+    "AZURE_STORAGE_ACCOUNT_NAME"      = azurerm_storage_account.conta_armazenamento.name
+    "FUNCTIONS_WORKER_RUNTIME"        = "python"
+    "SCM_DO_BUILD_DURING_DEPLOYMENT" = "true"
+    "ENABLE_ORYX_BUILD"               = "true"
+  }
 }
-
 
 
 
