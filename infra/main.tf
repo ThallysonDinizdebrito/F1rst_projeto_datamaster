0f2356f00f02411f094c76d192ad73f4aa06a7fa
ThallysonDinizdebrito <thallysonfdinizdebrito@gmail.com>
2025-09-14 00:26:58 -0300

    teste validar

--- infra/main.tf
+++ infra/main.tf
@@ -49,35 +49,6 @@ resource "azurerm_service_plan" "function_plan" {
   sku_name            = "Y1"
 }
 
-# ===========================
-# Function Apps (vazias) - func  
-# ===========================
-resource "azurerm_linux_function_app" "function_app" {
-  name                      = "${var.nome_do_grupo_de_recursos}-func"
-  location                  = azurerm_resource_group.grupo_principal.location
-  resource_group_name       = azurerm_resource_group.grupo_principal.name
-  service_plan_id           = azurerm_service_plan.function_plan.id
-  storage_account_name      = azurerm_storage_account.conta_armazenamento.name
-  storage_account_access_key= azurerm_storage_account.conta_armazenamento.primary_access_key
-
-  site_config {
-    application_stack {
-      python_version = "3.11"
-    }
-  }
-
-  identity { type = "SystemAssigned" }
-
-  app_settings = {
-    "RAW_CONTAINER_NAME"             = azurerm_storage_container.container_raw.name
-    "AZURE_STORAGE_ACCOUNT_NAME"     = azurerm_storage_account.conta_armazenamento.name
-    "FUNCTIONS_WORKER_RUNTIME"       = "python"
-    "SCM_DO_BUILD_DURING_DEPLOYMENT"= "true"
-    "ENABLE_ORYX_BUILD"              = "true"
-  }
-}
-
-
 
 
 # ========================================
