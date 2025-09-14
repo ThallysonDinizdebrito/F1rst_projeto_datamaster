93a358408b3075989f8505cd9d911c78558d56f3
ThallysonDinizdebrito <thallysonfdinizdebrito@gmail.com>
2025-09-14 02:18:10 -0300

    reteste

--- infra/main_eventgrid.tf
+++ infra/main_eventgrid.tf
@@ -11,35 +11,6 @@ resource "azurerm_eventgrid_system_topic" "raw_topic" {
 
 
 
-# Data source para Function App existente
-data "azurerm_linux_function_app" "function_validate" {
-  name                = "rg-dev-projeto-func-init"
-  resource_group_name = "rg-dev-projeto"
-}
-
-# EventGrid Subscription apontando para a Function
-resource "azurerm_eventgrid_event_subscription" "sub_func" {
-  name  = "sub-func-    "
-  scope = azurerm_eventgrid_topic.topic.id        # Ou storage_account.sa.id se for o caso
-
-  event_delivery_schema = "CloudEventSchemaV1_0"
-
-  retry_policy {
-    event_time_to_live    = 1440
-    max_delivery_attempts = 30
-  }
-
-  azure_function_endpoint {
-    function_id = "${data.azurerm_linux_function_app.function_validate.id}/functions/validate_fake_data"
-  }
-
-  lifecycle {
-    ignore_changes = [
-      azure_function_endpoint,
-    ]
-  }
-}
-
 
 
 # Data source para Function App existente
